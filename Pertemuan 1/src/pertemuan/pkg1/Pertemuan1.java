a7ab6e832c58dfc5f2d90e510cea2eda09fb7c22
PC-F-17 <dwichaandra2004@gmail.com>
2025-03-04 21:38:48 +0800

    Pertemuan 1

---
+++ Pertemuan 1/src/pertemuan/pkg1/Pertemuan1.java
@@ -0,0 +1,16 @@
+package pertemuan.pkg1;
+
+/**
+ *
+ * @author Chandra Dwi Cahyani
+ */
+public class Pertemuan1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.print ("Belajar Habasa Java");
+    }
+    
+}
